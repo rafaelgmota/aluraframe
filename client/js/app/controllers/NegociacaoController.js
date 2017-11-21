3a858c90d4e702e5f6ab595649e896ce8b26971e
Rafael Mota <faelmota@gmail.com>
2017-11-21 12:18:09 -0300

    Utilizando Proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,13 +1,38 @@
 class NegociacaoController {
     constructor(){
         let $ = document.querySelector.bind(document);
+        let self = this;
 
         this.inputData = $("#data");
         this.inputQuantidade = $("#quantidade");
         this.inputValor = $("#valor");
-        this._negociacoes = new ListaNegociacoes(model => //Arrow functions them escopo estático
-            this._negociacoesView.update(model)
-        );
+
+        //Aplicar proxy
+        this._negociacoes = new Proxy(new ListaNegociacoes(), {
+            //Quero interceptar funções, a chamada de funcoes primeiro faz um get e  depois um reflect
+           get(target, prop, receiver) {
+               console.log(typeof(prop));
+
+                //Verifica se a propriedade que esta sendo modifica é adiciona ou esvazia
+               if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+
+                   //Substitui o get padrão
+                   return function () {
+
+                       console.log(`interceptando: ${prop}`);
+
+                       //Chama a função original com os paramentros recebidos
+                       Reflect.apply(target[prop], target, arguments);
+
+                       //Chama o update de NegociacoesView, necessario o self
+                       //O target é o que o proxy esta encapsulando, no caso ListaNegociacoes.
+                       self._negociacoesView.update(target);
+                   }
+               }
+               //Se não for as funções desejadas, faz o get padrão
+               return Reflect.get(target, prop, receiver);
+           }
+        });
 
         this._negociacoesView = new NegociacoesView($("#negociacoes"));
         this._negociacoesView.update(this._negociacoes);
